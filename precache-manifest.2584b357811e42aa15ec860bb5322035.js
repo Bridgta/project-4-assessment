ec89c34046a138bb53cc3ce60ad4429b422d7e9d
Bridget Asser <bridgetasser@gmail.com>
2020-02-14 17:19:55 -0500

    Updates

---
+++ precache-manifest.2584b357811e42aa15ec860bb5322035.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf5896f40cc0f95a06e7f286001516a5",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "8a2181f938154f69f0d0",
+    "url": "/project-4-assessment/static/css/main.bda2caab.chunk.css"
+  },
+  {
+    "revision": "96ae9742c99a43ea2966",
+    "url": "/project-4-assessment/static/js/2.7c5025f9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/project-4-assessment/static/js/2.7c5025f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a2181f938154f69f0d0",
+    "url": "/project-4-assessment/static/js/main.d020f28c.chunk.js"
+  },
+  {
+    "revision": "9923933f7998bf1ba10e",
+    "url": "/project-4-assessment/static/js/runtime-main.6f46026f.js"
+  }
+]);
